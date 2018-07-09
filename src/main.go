58b0c1cbb76966eee5df602d4975db31b0899048
manan <aoy.mananchaya@gmail.com>
2018-07-09 15:03:23 +0700

    [Edited]: test Query DB

--- src/main.go
+++ src/main.go
@@ -7,8 +7,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type UsersData struct {
+	ID              int
+	CitizenID       string
+	Firstname       string
+	Lastname        string
+	BirthYear       int
+	FirstnameFather string
+	LastnamrFather  string
+	FirstnameMother string
+	LastnameMother  string
+	SoldierID       int
+	AddressID       int
+}
+
 func main() {
-	_, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
 
 	if err != nil {
 		fmt.Println("Connect fail")
@@ -17,4 +31,22 @@ func main() {
 	}
 
 	fmt.Println("Connet Success")
+
+	defer db.Close()
+
+	result, _ := db.Query("SELECT * FROM users")
+
+	for result.Next() {
+		var userData UsersData //เนื่องจากไม่ได้ประกาศเป็นอาร์เรย์ เลยให้วนใน for
+
+		err := result.Scan(&userData.ID, &userData.CitizenID, &userData.Firstname, &userData.Lastname,
+			&userData.BirthYear, &userData.FirstnameFather, &userData.LastnamrFather,
+			&userData.FirstnameMother, &userData.LastnameMother, &userData.SoldierID, &userData.AddressID)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(userData)
+	}
 }
